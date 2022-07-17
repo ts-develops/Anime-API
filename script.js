b71aff6a48370feef20fdd94ffe50278ca0b4a61
ts-develops <tawfiqrahman00@gmail.com>
2022-07-17 12:42:08 +0100

    fixing not displaying result

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const base_url = 'https://api.jikan.moe/v4/anime'
 
 
-function animeSearch(){
-    fetch (base_url)
-.then (result => result.json())
-.then(data => console.log(data))
-.catch (error => console.warn(error.message))
-}
+// function animeSearch(){
+//     fetch (base_url)
+// .then (result => result.json())
+// .then(data => console.log(data))
+// .catch (error => console.warn(error.message))
+// }
+const image = document.createElement("img")
+document.body.appendChild(image)
+
+const btn = document.querySelector('button')
+btn.addEventListener('click', pageLoad)
 
 function pageLoad(){
-
+fetch (base_url)
+.then (response => response.json())
+.then(json => {
+    image.src = json[0].title
+})
+.catch (error => console.log(error.message))
 }
 
-window.addEventListener('load', pageLoad)
-const click  = document.getElementById('button')
-click.addEventListener('click', animeSearch)
+// const click  = document.getElementById('button')
+
+
+//click.addEventListener('click', animeSearch)
