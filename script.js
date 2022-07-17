c04808d8da3ec4cea1c2d5e799d7d390b6d97fb9
ts-develops <tawfiqrahman00@gmail.com>
2022-07-17 14:46:52 +0100

    all data from array displaying

--- script.js
+++ script.js
@@ -10,11 +10,18 @@ return data.json()
 .then(completedata => {
     //accessing data to return specific array and information
     //console.log(completedata.data[2].title)
-    
-    let information =""
+
+    let information = " "
     //mapping to change whole data thats being grabbed
     completedata.data.map((values)=> {
-        information = ``
+        // using += lets data be displayed for all the API data(not one specific information)
+        information += `<div class="card" style="width: 20rem">
+        <img class="card-img-top" src="${values.images.jpg.image_url}" alt="anime" />
+        <div class="card-body text-center">
+          <h5 class="card-title">${values.title}</h5>
+        </div>
+      </div>`
+document.getElementById('cards-data').innerHTML= information
     })
 })
 //if none of the above work catch promise is displayed
